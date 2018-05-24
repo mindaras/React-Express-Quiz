858a83c28e983401d5cae9e45498b5d58dd885ca
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2018-05-24 16:58:41 +0300

    Indentation update

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Intro.css';
 import { Link } from 'react-router-dom';
 
-const Intro = (props) => (
+const Intro = () => (
   <div className="intro">
     <h1 className="intro__title">Quiz</h1>
     <Link to="/quiz" className="intro__button button">Begin</Link>
--- client/src/reducers/answersReducer.js
+++ client/src/reducers/answersReducer.js
@@ -1,7 +1,6 @@
 import { ADD_ANSWER, RESET_ANSWERS } from '../actions/types';
 
-export default function(state = [],
-  action) {
+export default function(state = [], action) {
   switch (action.type) {
     case ADD_ANSWER:
       return [...state, action.payload];
